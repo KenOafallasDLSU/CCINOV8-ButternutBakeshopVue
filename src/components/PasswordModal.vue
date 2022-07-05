45c99e2fb46dea35595c6e984e1e5aedf26ba606
AbiWnn <abigail_wan@dlsu.edu.ph>
2022-07-05 20:37:40 +0800

    CSS changes

--- src/components/PasswordModal.vue
+++ src/components/PasswordModal.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <b-modal class="confirm-modal" title="Sample Modal" ref="password-modal" v-b-modal.modal-center hide-header hide-header-close no-close-on-backdrop no-close-on-esc>
-      <label class="modal-label">Enter your Password</label>
-      <b-form-input v-model="password" type="password"></b-form-input>
-      <b-alert v-model="showPasswordAlert" variant="danger">Mali ang Binigay Password</b-alert>
+    <b-modal content-class="modal-content" class="confirm-modal" title="Sample Modal" ref="password-modal" v-b-modal.modal-center hide-header hide-header-close no-close-on-backdrop no-close-on-esc>
+      <div class="d-flex justify-content-end"><span class="text-white exit-button" @click="cancelModal">&times;</span></div>
+      <label class="modal-label text-white">Enter your Password</label>
+      
+      <b-form-input content-class="modal-input" v-model="password" type="password"></b-form-input>
+      <b-alert v-model="showPasswordAlert" variant="danger" class="mt-1">Incorrect Password</b-alert>
 
-      <div slot="modal-footer">
-        <b-button class="btn btn-primary" variant="danger" @click="cancelModal">Cancel</b-button>
-        <b-button class="btn btn-primary" variant="success" @click="checkPassword">Submit</b-button>
+      <div slot="modal-footer" class="d-flex justify-content-center">
+        <!--<b-button class="btn btn-primary" variant="danger" @click="cancelModal">Cancel</b-button>-->
+        <b-button class="confirm-button font-weight-bold" @click="checkPassword">Submit</b-button>
       </div>
     </b-modal>
 
@@ -58,5 +60,48 @@ export default {
 </script>
 
 <style scoped>
+
+/deep/ .modal-content {
+  background-color: #BD200F;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 100%; /* Could be more or less, depending on screen size */
+}
+
+/deep/ .modal-input {
+  width:100%;
+  height:45px;
+  border:none;
+  border-radius:5px;
+}
+
+/deep/ .modal-footer {
+  border:none !important;
+}
+
+/deep/ .confirm-button {
+  background-color:white !important;
+  border:none !important;
+  border-radius:5px !important;
+  height:35px !important;
+  color:black !important;
+}
+
+/deep/ .confirm-button:hover, .confirm-button:focus{
+  background-color:gray !important;
+  color:white !important;
+  transition-duration: 0.1s !important;
+  cursor: pointer !important;
+  text-decoration: none !important;
+}
+
+/deep/ .exit-button:hover {
+  cursor: pointer;
+  color:#888 !important;
+  transition-duration: 0.3s !important;
+}
+
+
 
 </style>
