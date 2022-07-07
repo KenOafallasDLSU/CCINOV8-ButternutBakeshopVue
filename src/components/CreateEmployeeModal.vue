708abc6a1c70bd5a2e8910ccc60322c4585bda35
Lorraine Cortel <lorraine_renee_cortel@dlsu.edu.ph>
2022-07-07 08:20:18 +0800

    updated modal in createemployee

--- src/components/CreateEmployeeModal.vue
+++ src/components/CreateEmployeeModal.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    <b-modal content-class="modal-content" class="confirm-modal" title="Sample Modal" ref="password-modal" v-b-modal.modal-center hide-header hide-header-close no-close-on-backdrop no-close-on-esc>
-      <label class="modal-label text-white">Employee is successfully added!</label>
+    <b-modal class="success-modal" ref="password-modal" v-model="showConfirmModal" v-b-modal.modal-center ok-only hide-header no-close-on-backdrop no-close-on-esc>
+      <bold>Employee successfully created!</bold>
       <br />
-      <label class="modal-label text-white">Here is their employee password: <b>{{ employeePassword }}</b></label>
-
-      <div slot="modal-footer" class="d-flex justify-content-center">
-        <b-button class="confirm-button font-weight-bold" @click="hideModal">Close</b-button>
-      </div>
+      <bold>Here is the employee password: {{ password }}</bold>
     </b-modal>
   </div>
 </template>
@@ -35,11 +31,13 @@ export default {
 <style scoped>
 
 /deep/ .modal-content {
-  background-color: #BD200F;
-  margin: 15% auto; /* 15% from the top and centered */
+  background-color: #fefefe;
+  margin: 40% auto; /* 15% from the top and centered */
   padding: 20px;
   border: 1px solid #888;
   width: 100%; /* Could be more or less, depending on screen size */
+  position:relative;
+  font-family: 'Montserrat' !important;
 }
 
 /deep/ .modal-input {
@@ -54,24 +52,52 @@ export default {
 }
 
 /deep/ .confirm-button {
-  background-color:white !important;
+  background:#BD200F !important;
   border:none !important;
   border-radius:5px !important;
   height:35px !important;
-  color:black !important;
+  color:white !important;
 }
 
-/deep/ .confirm-button:hover, .confirm-button:focus{
-  background-color:gray !important;
+/deep/ .confirm-button:hover {
+  background: #a01101 !important;
   color:white !important;
+  transition-duration: 0.1s !important;
+  cursor: pointer !important;
+  text-decoration: none !important;
+  border:none !important;
+}
+
+/deep/ .confirm-button:active,
+.confirm-button:focus,
+.confirm-button:visited {
+    background: #a01101 !important;
+    border:0 !important;
+    box-shadow: 0 0 0 3px rgba(255, 90, 90, 0.723) !important;
+}
+
+/deep/ .cancel-button {
+  border:none !important;
+  background: #e8e8e8;
+  color:black;
+  border-radius:5px !important;
+  height:35px !important;
+}
+
+/deep/ .cancel-button:hover{
+  border:none !important;
+  color:black;
+  background: #cccccc !important;
   transition-duration: 0.1s !important;
   cursor: pointer !important;
   text-decoration: none !important;
 }
 
-/deep/ .exit-button:hover {
-  cursor: pointer;
-  color:#888 !important;
-  transition-duration: 0.3s !important;
+/deep/ .cancel-button:active,
+.cancel-button:focus,
+.cancel-button:visited {
+    background: #cccccc !important;
+    border:0 !important;
+    box-shadow: 0 0 0 3px rgba(169, 169, 169, 0.723) !important;
 }
 </style>
